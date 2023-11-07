b2a58ab54efaeaefd8c3d0d82df891eb3f512570
libinali <ali.libin@yahoo.com>
2023-11-07 05:22:04 +0000

    First commit

--- vampire.js
+++ vampire.js
@@ -24,14 +24,14 @@ class Vampire {
     let numOfVampires = 0;
 
     let currentVampire = this;
-    
+
     while (currentVampire.creator) {
       numOfVampires++;
       currentVampire = currentVampire.creator;
     }
 
     return numOfVampires;
-    
+
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
@@ -39,15 +39,56 @@ class Vampire {
     return vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal;
   }
 
-  /** Stretch **/
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
 
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+    if (this.name === name) {
+      return this;
+    } 
 
+    for (const vampire of this.offspring) {
+      
+      const foundVampire = vampire.vampireWithName(name);
+      if (foundVampire) {
+        return foundVampire;
+      } 
+    }
+
+    return null;
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let total = 0;
+
+
+    for (const vampire of this.offspring) {
+      total += 1;
+      total += vampire.totalDescendents;
+    }
+
+    return total;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    const converted = [];
+  
+    if (this.yearConverted > 1980) {
+      converted.push(this);
+    }
+  
+    for (const vampire of this.offspring) {
+      const offspringConverted = vampire.allMillennialVampires;
+  
+      for (const convertedVampire of offspringConverted) {
+        if (convertedVampire.yearConverted > 1980) {
+          converted.push(convertedVampire);
+        }
+      }
+    }
+  
+    return converted;
   }
 }
 
